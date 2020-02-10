067c89a64c3bf8e2f682e5f2b7bc023b76a3b8b3
Cameron Haseyama <haseyama@usc.edu>
2020-02-09 17:48:08 -0800

    sample export function

--- routes/workspace.js
+++ routes/workspace.js
@@ -1,3 +1,17 @@
 /***
 *	Workspace Page Handlers 
 */
+const express = require("express");
+const admin = require('firebase-admin');
+
+exports.getUsers = function(db) {
+	db.collection('users').get()
+	  .then((snapshot) => {
+	    snapshot.forEach((doc) => {
+	      console.log(doc.id, '=>', doc.data());
+	    });
+	  })
+	  .catch((err) => {
+	    console.log('Error getting documents', err);
+	});
+}
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const admin = require('firebase-admin');
 const app = express();
 const port = 3000;
 
-const admin = require('firebase-admin');
-
-let serviceAccount = require('firebase-key.json');
+let serviceAccount = require('./firebase-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 let db = admin.firestore();
+
+//Test Database Connection
+const workspace = require('./routes/workspace.js');
+workspace.getUsers(db);
 
 app.get('/', (req, res) => res.send('Capstone Server!'));
 
