f71b0e07ba3d4baf27e117250602cb85b1265e55
Alice Lee <alicesle@usc.edu>
2020-02-09 16:25:12 -0800

    added firebase admin

--- routes/server.js
+++ routes/server.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Capstone Server!'))
+const admin = require('firebase-admin');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+let serviceAccount = require('path/to/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+
+app.get('/', (req, res) => res.send('Capstone Server!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
