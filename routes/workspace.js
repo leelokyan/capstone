df7f82d1ecd11ed4cf4fd6be1d24fac7d1f95d0c
Cameron Haseyama <haseyama@usc.edu>
2020-02-27 00:07:33 -0800

    wrote and tested workspace endpoints

--- routes/workspace.js
+++ routes/workspace.js
@@ -16,19 +16,97 @@ exports.getUsers = function(db) {
 
 /***************************
 	Create Workspace:
-		Request - (string:name,string:authCode)
-		Response - (bool:success)
+		Request - (string:workspaceName,string:authCode)
+		Response - (int:workspaceId)
 ***************************/
 router.post('/create_workspace', function(req,res){
+	console.log("create_workspace");
+	//Parse Request Body
+	var workspaceName = req.body.workspaceName;
+	var authCode = req.body.authCode;
 
+	//Check with frontend team degree of backend validation necessary
+	if(!workspaceName || !authCode){	
+		workspaceId = -1;
+	}else{
+		console.log("Workspace Name: " + workspaceName);
+		console.log("AuthCode: " + authCode);
+		/***
+		* Insert Database API Call
+		*/
+		workspaceId = 1;
+	}	
+	var response = {
+		'workspaceId' : workspaceId
+	};
+	res.json(response);
 });
 
 /***************************
 	Join Workspace:
-		Request - (int:workspaceId,string:authCode)
-		Response - (bool:success)
+		Request - (int:userId,int:workspaceId,string:authCode)
+		Response - (bool:joinSuccess)
 ***************************/
 router.post('/join_workspace', function(req,res){
+	console.log("join_workspace");
+	//Parse Request Body
+	var userId = req.body.userId;
+	var workspaceId = req.body.workspaceId;
+	var authCode = req.body.authCode;
+	var joinSuccess;
 
+	//Note: check desired security measures for passing authCode, can encrypt
+	if(!userId || !workspaceId || !authCode){
+		joinSuccess = false;
+	}else{
+		console.log("User Id: " + userId);
+		console.log("Workspace Id: " + workspaceId);
+		console.log("AuthCode: " + authCode);
+		/***
+		* Insert Database API Call
+		*/
+		joinSuccess = true;
+	}
+	var response = {
+		'joinSuccess' : joinSuccess
+	};
+	res.json(response);
 });
 
+/***************************
+	Get My Workspace:
+		Request - (int:userId)
+		Response - (Object [] : myWorkspaceList (or empty list)
+					- int : workspaceId
+					- String : workspaceName)
+***************************/
+router.post('/get_my_workspace', function(req,res){
+	console.log("get_my_workspace");
+	//Parse Request Body
+	var userId = req.body.userId;
+	var myWorkspaceList;
+
+	if(!userId){
+		myWorkspaceList = [];
+	}else{
+		console.log("User Id: " + userId);
+		/***
+		* Insert Database API Call
+		*/
+		w1 = {
+			workspaceId : 1,
+			workspaceName : 'Krusty Crab'
+		};
+		w2 = {
+			workspaceId : 2,
+			workspaceName : 'Chum Bucket'
+		};
+		myWorkspaceList = [w1,w2];
+	}
+	var response = {
+		'myWorkspaceList' : myWorkspaceList
+	};
+	res.json(response);
+});
+
+module.exports = router;
